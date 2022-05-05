bdc9ad22b4eb0973a2e69da6152bb14c23b7e43b
sunayanapandra <pandrasunayana@gmail.com>
2022-05-05 17:44:58 +0530

    added src pom.xml

---
+++ src/main/java/com/groceryapp/GroceryappProductMsApiApplication.java
@@ -0,0 +1,14 @@
+package com.groceryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryappProductMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryappProductMsApiApplication.class, args);
+		System.out.println("product ms working.....");
+	}
+
+}
